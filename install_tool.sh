1c8a5fe307e20e574f879c5ed08d39075136a43c
Danny <95729902+datboyblu3@users.noreply.github.com>
2023-08-15 08:54:23 +0300

    Update install_tool.sh

--- install_tool.sh
+++ install_tool.sh
@@ -27,6 +27,7 @@ yes | sudo apt-get install asciinema
 yes | sudo apt-get install -y --no-install-recommends xz-utils liblz4-tool musl-tools
 yes | sudo apt-get install duf
 yes | bash <(curl -sL https://raw.githubusercontent.com/denisidoro/navi/master/scripts/install)
+yes | sudo apt install gdebi -y
 
 echo "Restart machine in 3...2...1.."
 yes | sudo reboot
